aa8d12fe1058dabc6a13c87fb3d5dd51c4b95d1e
mikaijun <mj.tempe443@gmail.com>
2024-10-19 14:03:48 +0900

    Prisma環境構築

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client'
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const prisma = new PrismaClient()
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express with TypeScript!');
+});
+
+app.post('/user', async (req: Request, res: Response) => {
+  const newUser = await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io'
+    }
+  })
+  res.status(201).json(newUser);
 });
 
 app.listen(port, () => {
